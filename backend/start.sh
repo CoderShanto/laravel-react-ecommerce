7f802def3e1609969874f58c664bd7704944ebec
Mahmud Hasan Shanto <sshanto12344321@gmail.com>
2026-02-23 06:39:43 +0600

    fixes env and route cache

--- backend/start.sh
+++ backend/start.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# Cache config (optional but good)
 php artisan config:cache || true
-php artisan route:cache || true
+# IMPORTANT: do NOT cache routes while debugging deployment
+# php artisan route:cache || true
 php artisan view:cache || true
 
-
-
-# Start php-fpm in background
 php-fpm -D
-
-# Start nginx
 nginx -g "daemon off;"
